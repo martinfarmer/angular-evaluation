88656a94b11c630eb4f42f9e6dd86ac41888b34c
Andrew Cuga <andrew@kalibrilabs.com>
2017-08-10 07:36:26 -0400

    Adding todosaver component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoSaverComponent } from './todo-saver/todo-saver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoSaverComponent
   ],
   imports: [
     BrowserModule
