650c20975d88fe4844af9763210e0221c063f12a
Andrew Cuga <andrew@kalibrilabs.com>
2017-08-10 09:39:11 -0400

    Adding method to save and get all todos to service

--- src/app/services/repository.service.ts
+++ src/app/services/repository.service.ts
@@ -3,6 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RepositoryService {
 
+  private todoItems : string[];
+
   constructor() { }
+
+  saveTodo(todoToSave: string) {
+    if (todoToSave) {
+      this.todoItems.push(todoToSave);
+    }
+  }
+
+  getTodos():string[] {
+    return this.todoItems;
+  }
 
 }
